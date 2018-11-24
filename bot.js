b3164b918da96d9f4faa9b1f44dd320348d2bb97
ma70 <45004299+ma70@users.noreply.github.com>
2018-11-24 22:20:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,15 +105,49 @@ client.on("guildMemberAdd", member => {
 
 
 });
-    member.guild.fetchInvites().then(guildInvites => {
-      const ei = guildInvites[member.guild.id];
-      const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-      const inviter = client.users.get(invite.inviter.id);
-       welcomer.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-    }); 
-  });
-     })
-    }})
+               
+ var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفرك هنا")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'اسم روم اللي برسل فيها');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "اسم سرفرك هنا")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
+    
 
 
 client.login(process.env.BOT_TOKEN);
